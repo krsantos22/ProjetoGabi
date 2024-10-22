bafc4bed658f376b84a8d42baa7a17f7226a3e06
krsantos22 <kaique.ramos@acct.global>
2024-10-22 12:25:44 -0300

    testando 2

--- cypress/e2e/teste gabriela.cy.js
+++ cypress/e2e/teste gabriela.cy.js
@@ -23,24 +23,24 @@ describe('teste site da automate exercise', () => {
       .should('be.visible')
   });
 
-  it('falha ao cadastrar um cliente no site da automate exercise', () => {
-    // Adicionando o título para o Allure
-    cy.allure().title('Cadastro de Cliente');
+  // it('falha ao cadastrar um cliente no site da automate exercise', () => {
+  //   // Adicionando o título para o Allure
+  //   cy.allure().title('Cadastro de Cliente');
 
-    cy.get('[data-qa="signup-name"]')
-      .click()
-      .type('Gabriela');
+  //   cy.get('[data-qa="signup-name"]')
+  //     .click()
+  //     .type('Gabriela');
 
-    cy.get('[data-qa="signup-email"]')
-      .click()
-      .type('gabriela.teste@gmail.com');
+  //   cy.get('[data-qa="signup-email"]')
+  //     .click()
+  //     .type('gabriela.teste@gmail.com');
 
-    cy.get('[data-qa="signup-button"]')
-      .click();
+  //   cy.get('[data-qa="signup-button"]')
+  //     .click();
 
-    // Verificação se o cadastro foi bem-sucedido
-    cy.get('.signup-form > form > ')
-      .should('be.visible') 
-  });
+  //   // Verificação se o cadastro foi bem-sucedido
+  //   cy.get('.signup-form > form > ')
+  //     .should('be.visible') 
+  // });
 
 });
